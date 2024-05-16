e878c1eba9c5d1243ee2dc347bb3f697014c9745
leoboyerbx <contact@leoboyer.fr>
2024-05-16 17:28:10 +0200

    wip download page

--- website/src/components/DownloadButton.vue
+++ website/src/components/DownloadButton.vue
@@ -27,20 +27,37 @@ const downloadData: Record<string, {
         icon: 'i-uil-apple',
         download: props.releaseData.assets.find((asset: any) => asset.name.endsWith(".dmg"))?.browser_download_url,
         description: '.dmg universel'
-    }
+    },
+    windows: {
+        icon: 'i-uil-windows',
+        download: props.releaseData.assets.find((asset: any) => asset.name.endsWith(".exe"))?.browser_download_url,
+        description: 'Installateur .exe'
+    },
+    linux: {
+        icon: 'i-uil-linux',
+        download: props.releaseData.assets.find((asset: any) => asset.name.endsWith(".AppImage"))?.browser_download_url,
+        description: '.AppImage'
+    },
 } as const
 
 const activeDownloadData = computed(() => downloadData[os.value])
+const otherDownloads = computed(() => {
+    const o = { ...downloadData }
+    delete o[os.value]
+    return o
+})
+console.log(otherDownloads.value)
 </script>
 <template>
     <div>
         <div class="flex gap-2 relative items-stretch">
-            <a class="btn btn-primary text-white flex items-center px-6 py-4 h-auto">
+            <a :href="activeDownloadData.download"
+               class="btn btn-primary text-white flex items-center px-6 py-4 h-auto">
                 <span class="text-[20px] mb-0.5" :class="activeDownloadData.icon" />
                 <span class="w-px bg-current h-8 mx-2"></span>
                 <span class="flex flex-col gap-1">
                     <span class="text-lg leading-none">
-                        Télécharger
+                        Télécharger pour <span class="capitalize">{{ os }}</span>
                     </span>
                     <span class="font-light text-xs">
                         {{ activeDownloadData.description }}
@@ -54,8 +71,20 @@ const activeDownloadData = computed(() => downloadData[os.value])
                           class="i-uil-angle-down text-[48px] mt-0.5"></span>
                 </div>
                 <ul tabindex="0" class="dropdown-content z-[1] menu p-2 mt-2 shadow bg-base-300 rounded-box w-52">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
+                    <li v-for="(download, k) in otherDownloads" :key="k">
+                        <a :href="download.download" class="flex items-center">
+                            <span class="text-[20px] mb-0.5" :class="download.icon" />
+                            <span class="w-px bg-current h-6 mx-2"></span>
+                            <span class="flex flex-col gap-1">
+                                <span class="text-lg leading-none capitalize">
+                                    {{ k }}
+                                </span>
+                                <span class="font-light text-xs">
+                                    {{ download.description }}
+                                </span>
+                            </span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
